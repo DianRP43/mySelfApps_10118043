3bca419574e68e3045943b299fee90aa71399112
DianRP43 <dianrosapratama2000@gmail.com>
2021-05-21 00:55:48 -0800

    update baru lagi

--- app/src/main/java/com/example/quiz/Music.java
+++ app/src/main/java/com/example/quiz/Music.java
@@ -13,87 +13,98 @@ import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class Music extends AppCompatActivity {
-    Context contextVideo,context;
+    Context contextVideo, context;
     RecyclerView recyclerViewVideo, recyclerViewMusic;
-    RecyclerView.Adapter recyclerViewAdapterVideo,recyclerViewAdapterMusic;
-    RecyclerView.LayoutManager recylerViewLayoutManagerVideo,recylerViewLayoutManagerMusic;
+    RecyclerView.Adapter recyclerViewAdapterVideo, recyclerViewAdapterMusic;
+    RecyclerView.LayoutManager recylerViewLayoutManagerVideo, recylerViewLayoutManagerMusic;
     VideoView videoView;
     String[] subjectValuesVideo = {
-            "Dat Stick","All of me","Till It Hurts"
+            "Dat Stick", "All of me", "Till It Hurts"
     };
     String[] subjectUrlVideo;
     String[] subjectValuesMusic = {
-            "Dat Stick","All of me","Till It Hurts","Dat Stick","All of me","Till It Hurts",
-            "Dat Stick","All of me","Till It Hurts"
-    };
+            "Girls Like You - Maroon 5",
+            "Style - Taylor Swift",
+            "Perfect - Ed Sheeran",
+            "Just Give Me A Reason - Pink",
+            "Map - Maroon 5",
+            "To Good At Goodbyes - Sam Smiths",
+            "All I Ask - Adele",
+            "Photograph - Ed Sheeran",
+            "Someone Like You - Adele",
+            "Happier - Ed Sheeran",
+            "Treat You Better - Shawn Mendes",
+            "Thinking Out Loud - Ed Sheeran",
+            "Payphone - Maroon 5"
+};
     DrawerLayout drawerLayout;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+@Override
+protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
-        subjectUrlVideo = new String[]{
-                "android.resource://" + getPackageName() + "/" + R.raw.video,
-                "android.resource://" + getPackageName() + "/" + R.raw.video2,
-                "android.resource://" + getPackageName() + "/" + R.raw.video3
+        subjectUrlVideo=new String[]{
+        "android.resource://"+getPackageName()+"/"+R.raw.video,
+        "android.resource://"+getPackageName()+"/"+R.raw.video2,
+        "android.resource://"+getPackageName()+"/"+R.raw.video3
         };
-        contextVideo = getApplicationContext();
-        recyclerViewVideo = findViewById(R.id.recyclerViewVideo);
-        recylerViewLayoutManagerVideo = new LinearLayoutManager( this);
+        contextVideo=getApplicationContext();
+        recyclerViewVideo=findViewById(R.id.recyclerViewVideo);
+        recylerViewLayoutManagerVideo=new LinearLayoutManager(this);
         //context,LinearLayoutManager.HORIZONTAL,false
         recyclerViewVideo.setLayoutManager(recylerViewLayoutManagerVideo);
-        recyclerViewAdapterVideo = new AdapterRecycleViewVideo(subjectValuesVideo,subjectUrlVideo,this);
+        recyclerViewAdapterVideo=new AdapterRecycleViewVideo(subjectValuesVideo,subjectUrlVideo,this);
         recyclerViewVideo.setAdapter(recyclerViewAdapterVideo);
 
-        context = getApplicationContext();
-        recyclerViewMusic = findViewById(R.id.recyclerViewMusic);
-        recylerViewLayoutManagerMusic = new LinearLayoutManager(context);
+        context=getApplicationContext();
+        recyclerViewMusic=findViewById(R.id.recyclerViewMusic);
+        recylerViewLayoutManagerMusic=new LinearLayoutManager(context);
         //context,LinearLayoutManager.HORIZONTAL,false
         recyclerViewMusic.setLayoutManager(recylerViewLayoutManagerMusic);
-        recyclerViewAdapterMusic = new AdapterRecycleViewMusic(subjectValuesMusic,context);
+        recyclerViewAdapterMusic=new AdapterRecycleViewMusic(subjectValuesMusic,context);
         recyclerViewMusic.setAdapter(recyclerViewAdapterMusic);
 
         //ass
-        drawerLayout = findViewById(R.id.DrawerLayout);
-    }
-    public void ClickMenu(View view){
+        drawerLayout=findViewById(R.id.DrawerLayout);
+        }
+public void ClickMenu(View view){
         //Open drawer
         HomeActivity.openDrawer(drawerLayout);
 
-    }
-    public void ClickLogo(View view){
+        }
+public void ClickLogo(View view){
         HomeActivity.closeDrawer(drawerLayout);
-    }
-    public void ClickHome(View view){
+        }
+public void ClickHome(View view){
         //redirect to home
         HomeActivity.redirectActivity(this,HomeActivity.class);
-    }
-    public void ClickGallery(View view){
+        }
+public void ClickGallery(View view){
         //redirect to gallery
         HomeActivity.redirectActivity(this,Gallery.class);
-    }
+        }
 
-    public void ClickDaily(View view){
+public void ClickDaily(View view){
         //redirect to gallery
         HomeActivity.redirectActivity(this,Daily.class);
-    }
-    public void ClickMusic(View view){
+        }
+public void ClickMusic(View view){
 
         //recreate
         recreate();
-    }
-    public void ClickProfile(View view){
+        }
+public void ClickProfile(View view){
         //redirect to profile
         HomeActivity.redirectActivity(this,Profile.class);
-    }
-    public void ClickLogout(View view){
+        }
+public void ClickLogout(View view){
         //close
         HomeActivity.logout(this);
-    }
+        }
 
-    @Override
-    protected void onPause() {
+@Override
+protected void onPause(){
         super.onPause();
         //close drawer
         HomeActivity.closeDrawer(drawerLayout);
-    }
-}
+        }
+        }
--- app/src/main/java/com/example/quiz/Profile.java
+++ app/src/main/java/com/example/quiz/Profile.java
@@ -3,19 +3,55 @@ package com.example.quiz;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Profile extends AppCompatActivity {
+
+    Button email, instagram, location;
+
 
     DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        instagram = findViewById(R.id.btn_instagram);
+
+        instagram.setOnClickListener(new View.OnClickListener(){
+
+
+            @Override
+            public void onClick(View v) {
+                gotoUrl("https://www.instagram.com/dianrosapratama/");
+            }
+        });
+
+        location = findViewById(R.id.btn_findme);
+
+        location.setOnClickListener(new View.OnClickListener(){
+
+
+            @Override
+            public void onClick(View v) {
+                gotoUrl("https://goo.gl/maps/4DsPTJeqfyopwSVf7");
+            }
+        });
         //ass
         drawerLayout = findViewById(R.id.DrawerLayout);
     }
+
+    private void gotoUrl(String s) {
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
+    }
+
     public void ClickMenu(View view){
         //Open drawer
         HomeActivity.openDrawer(drawerLayout);
@@ -58,4 +94,46 @@ public class Profile extends AppCompatActivity {
         //close drawer
         HomeActivity.closeDrawer(drawerLayout);
     }
+
+    public void ClickAbout(View view){
+        //redirect to Gallery
+        About(this);
+    }
+
+    public static void About(final Activity activity) {
+        //alert
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        //set title
+        builder.setTitle("About");
+        //set message
+        builder.setMessage("dip v0.01Aplikasi ini memuat data data pribadi tentang Dian Rosa Pratama ");
+
+        builder.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                //dismiss dialog
+                dialog.dismiss();
+
+            }
+
+        });
+        //show dialog
+        builder.show();
+    }
+    public void onButtonTap(View v){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"dianrosapratama2000@gmail.com"});
+        intent.putExtra(Intent.EXTRA_CC, new String[] {""});
+        intent.putExtra(Intent.EXTRA_SUBJECT, "");
+        intent.putExtra(Intent.EXTRA_TEXT, "");
+
+        try {
+            startActivity(Intent.createChooser(intent, null));
+        } catch (android.content.ActivityNotFoundException ex) {
+            //do something else
+        }
+    }
 }
+
